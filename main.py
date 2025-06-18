5e28f5c07fb024dcbca6382de8e2bc0d4bd69713
Hassan Mustafa <hassanmustafa030107@gmail.com>
2025-06-18 20:44:33 +0800

    added event handling so the window could open, set the framerate to 120 fps

--- main.py
+++ main.py
@@ -4,6 +4,18 @@ pygame.init()
 
 WINDOW_WIDTH = 800
 WINDOW_HEIGHT = 600
+FPS = 120
 
 Window = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
 pygame.display.set_caption("Falling Sand")
+
+clock = pygame.time.Clock()
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+            
+    pygame.display.flip()
+    clock.tick(FPS)
