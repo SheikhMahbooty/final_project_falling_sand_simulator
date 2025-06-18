965bbfdd3b194f75ac5b1bfdef0dcd6aa4721438
Hassan Mustafa <hassanmustafa030107@gmail.com>
2025-06-18 20:51:13 +0800

    created how the cells are sized with columns, rows, and cell size

--- grid.py
+++ grid.py
@@ -1,3 +1,7 @@
 class Grid:
-    def __init__(self):
+    def __init__(self, width, height, cell_size):
+        self.rows = height // cell_size
+        self.columns = width // cell_size
+        self.cell_size = cell_size
+        self.cells = [[None for _ in range(self.columns)] for _ in range (self.rows)]
         
